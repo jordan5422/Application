7d9cdff459601154b12f62e53d217e792c352bd9
jordan5422 <tatuefom@3il.fr>
2024-02-08 08:11:08 +0000

    jordan

--- base/header.php
+++ base/header.php
@@ -32,3 +32,26 @@
   </div>
 </nav>
 <script src="https://kit.fontawesome.com/b2ba396bc9.js" crossorigin="anonymous"></script>
+
+<nav class="navbar">
+      <div class="nav-center">
+        <div class="nav-header">
+          <a href="index.html" class="nav-logo">
+            <img src="./assets/logo.svg" alt="simply recipes" />
+          </a>
+          <button class="nav-btn btn">
+            <i class="fas fa-align-justify"></i>
+          </button>
+        </div>
+        <div class="nav-links">
+          <a href="index.html" class="nav-link"> home </a>
+          <a href="about.html" class="nav-link"> about </a>
+          <a href="tags.html" class="nav-link"> tags </a>
+          <a href="recipes.html" class="nav-link"> recipes </a>
+
+          <div class="nav-link contact-link">
+            <a href="contact.html" class="btn"> contact </a>
+          </div>
+        </div>
+      </div>
+    </nav>
